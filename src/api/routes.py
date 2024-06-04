0546695c7bfbc9b750d31334baec3822a1b3cfc1
santiagommarino <153780787+santiagommarino@users.noreply.github.com>
2024-06-04 00:10:26 +0000

    moving codespace

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+import hashlib
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +23,29 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/singup', methods=(['POST']))
+def singup():
+    body = request.get_json()
+    user_email = body['email']
+    user_password = hashlib.sha256(body['password'].encode('utf-8'))hexdigest()
+    user = User(email=user_email, password=user_password)
+    db.session.add(user)
+    db.session.commit()
+
+@api.route('/login', methods=(['POST']))
+def login():
+    body = request.get_json()
+    user_email = body['email']
+    user_password = hashlib.sha256(body['password'].encode('utf-8'))hexdigest()
+    user = User.query.filter_by(email=user_email). first()
+    if user and user.password = user_password
+        access_token = create_access_token(identity=user.id)
+        return jsonify(access_token)
+
+@api.route('/user', methods=(['GET']))
+@jwt_required()
+def get_user():
+    user_id = get_jwt_identity()
+    user = User.query.get(get_user)
+    return jsonify(email=user.email, name=user.name)
